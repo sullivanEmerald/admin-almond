7bc746a248e1c2b340374b7f5ec4e9090d4ed1de
Sullivan Amadike <amadikesullivan20@gmail.com>
2025-01-25 15:41:28 +0100

    added the category folder

--- src/admin/dashboard/pages/home.tsx
+++ src/admin/dashboard/pages/home.tsx
@@ -7,6 +7,8 @@ const Dashboard = () => {
         <Category image='images/dashboard/main.png' />
         <Category image='images/dashboard/home.png' />
         <Category image='images/dashboard/groceries.png' />
+        <Category image='images/dashboard/laptop.png' />
+        <Category image='images/dashboard/electronics.png' />
         <Category image='images/dashboard/phone.png' />
       </div>
     </>
